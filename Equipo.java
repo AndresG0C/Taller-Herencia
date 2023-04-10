6a68d11dadd8d92d5e39d1d6acdd82d777cc81c1
AndresG0C <126472795+AndresG0C@users.noreply.github.com>
2023-04-10 07:28:45 -0500

    Codificacion del punto 1

---
+++ Equipo.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class Equipo {
+    
+    private int id;
+    private String Nombre;
+    private String Apellidos;
+    private int Edad;
+
+    public Equipo() {
+    }
+
+    public Equipo(int id, String Nombre, String Apellidos, int Edad) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.Edad = Edad;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Apellidos
+     */
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    /**
+     * @param Apellidos the Apellidos to set
+     */
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo: " + "id= " + id + ", Nombre= " + Nombre + ", Apellidos= " + Apellidos + ", Edad= " + Edad;
+    }    
+}
